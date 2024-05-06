507fa7bb168c659011db1addcf358898b32131b4
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-05-05 23:14:30 -0400

    init

---
+++ src/Switch/index.tsx
@@ -0,0 +1,31 @@
+import React, { ReactElement, PropsWithChildren } from "react";
+
+export interface SwitchProps<T> {
+  /** The value to test */
+  test: T;
+  children: ReactElement<CaseProps<T>>[];
+}
+
+/**
+ * A React-friendly implementation of switch/case logic. This component is tightly coupled with the Case component, which is to be used as a child that contains the elements needed to render.
+ * An example of this in use can be found in `ViewsTab.tsx`.
+ */
+export const Switch = <T,>({ test, children }: SwitchProps<T>) => {
+  const defaultResult = children.find((child) => child.props.default) || null;
+  const result = children.find((child) => child.props.value === test);
+
+  return result || defaultResult;
+};
+
+export interface CaseProps<T> extends PropsWithChildren {
+  /** The value to match. Not used directly in this component. */
+  value: T;
+  /** Indicates if this case should be the default. Not used directly in this component. */
+  default?: boolean;
+}
+
+/**
+ * A React-friendly implementation of switch/case logic. This component is tightly coupled with the Switch component, which should be a parent of this component.
+ * An example of this in use can be found in `ViewsTab.tsx`.
+ */
+export const Case = <T,>({ children }: CaseProps<T>) => <>{children}</>;
