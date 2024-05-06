00d22603540c1ddb8aa15c8bd0c19f5f5801104c
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-05-06 18:00:45 -0400

    uses wildcard

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig((options) => ({
-  entryPoints: ["src/button.tsx", "src/if.tsx"],
+  entryPoints: ["src/*.tsx"],
   format: ["cjs", "esm"],
   dts: true,
   sourcemap: true,
