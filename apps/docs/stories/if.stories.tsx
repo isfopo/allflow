247cd43eb974c119349c6714bb017a484f88b0bb
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-05-07 07:17:47 -0400

    adds story for if

---
+++ apps/docs/stories/if.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { If } from "@acme/ui/if";
+
+const meta: Meta<typeof If> = {
+  component: If,
+  argTypes: {
+    is: {
+      control: { type: "boolean" },
+    },
+    el: {
+      control: { disabled: true },
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof If>;
+
+export const Primary: Story = {
+  render: (props) => (
+    <If {...props}>
+      <p>True</p>
+    </If>
+  ),
+  name: "If",
+  args: {
+    is: true,
+    el: <p>False</p>,
+  },
+  argTypes: { el: { control: { disable: true } } },
+};
