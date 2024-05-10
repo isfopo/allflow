4f2590f6ef89c3b5a383582a53ebf9c6fb441fa0
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-05-10 00:18:19 -0400

    adds default component

--- packages/ui/src/switch.tsx
+++ packages/ui/src/switch.tsx
@@ -3,34 +3,56 @@ import type { ReactElement, PropsWithChildren } from "react";
 export interface SwitchProps<T> {
   /** The value to test */
   test: T;
-  children: ReactElement<CaseProps<T>>[];
+  children: ReactElement<OptionProps<T>>[];
 }
 
 /**
  * A React-friendly implementation of switch/case logic. This component is tightly coupled with the Case component, which is to be used as a child that contains the elements needed to render.
- * An example of this in use can be found in `ViewsTab.tsx`.
  */
 export const Switch = <T,>({
   test,
   children,
-}: SwitchProps<T>): ReactElement<CaseProps<T>> | null => {
+}: SwitchProps<T>): ReactElement<CaseProps<T> | DefaultProps> | null => {
   const defaultResult = children.find((child) => child.props.default) || null;
   const result = children.find((child) => child.props.value === test);
 
   return result || defaultResult;
 };
 
+export interface OptionProps<T> extends PropsWithChildren {
+  /** The value to match. Not used directly in this component. */
+  value?: T;
+  /** The default value to match. Not used directly in this component. */
+  default?: boolean;
+}
+
 export interface CaseProps<T> extends PropsWithChildren {
   /** The value to match. Not used directly in this component. */
   value: T;
-  /** Indicates if this case should be the default. Not used directly in this component. */
-  default?: boolean;
+}
+
+export type DefaultProps = PropsWithChildren;
+
+/**
+ * A React-friendly implementation of switch/case logic. This component is tightly coupled with the Switch component, which should be a parent of this component.
+ * If you want to render a default case, use the `Default` component.
+ */
+function Option<T>({ children }: OptionProps<T>): ReactElement {
+  return <>{children}</>;
 }
 
 /**
  * A React-friendly implementation of switch/case logic. This component is tightly coupled with the Switch component, which should be a parent of this component.
- * An example of this in use can be found in `ViewsTab.tsx`.
+ * If you want to render a default case, use the `Default` component.
  */
-export function Case<T>({ children }: CaseProps<T>): ReactElement {
-  return <>{children}</>;
+export function Case<T>({ children, value }: CaseProps<T>): ReactElement {
+  return <Option value={value}>{children}</Option>;
+}
+
+/**
+ * A React-friendly implementation of switch/case logic. This component is tightly coupled with the Switch component, which should be a parent of this component.
+ * This is the default case.
+ */
+export function Default({ children }: PropsWithChildren): ReactElement {
+  return <Option default>{children}</Option>;
 }
