fb9cec21bef4ee0eddbb648121e12a82b2fa006d
Isaac Poole <isaacpoolemusic@gmail.com>
2025-03-01 02:39:00 -0500

    🔧 chore(package): move index.tsx to root directory
    - rename index.tsx from src to root for better structure

---
+++ package/index.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+export { Each, type EachProps } from "./src/Each";
+export { If, type IfProps, Not } from "./src/If";
+export { Switch, type SwitchProps, Case, type CaseProps } from "./src/Switch";
