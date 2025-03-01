6a8fc550520468ed3634b33d5940b4bf4206b4e0
Isaac Poole <isaacpoolemusic@gmail.com>
2025-03-01 16:07:54 -0500

    👷 ci(workflows): add dependency installation step
    - add pnpm install step in release workflow for dependency management
    
    💄 style(package): remove unused React import
    
    - cleanup unused React import in index.tsx file to improve code clarity

--- package/index.tsx
+++ package/index.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 export { Each, type EachProps } from "./src/Each";
 export { If, type IfProps, Not } from "./src/If";
 export { Switch, type SwitchProps, Case, type CaseProps } from "./src/Switch";
